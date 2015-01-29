2e9e56bfe671566ba6fe8539cf584524f6f99304
Mendolis <mendolis@live.com>
2015-01-28 21:47:44 -0600

    Firming up the example files
    Changes have been made to module.  Insure examples work with changes and
    comment a bit better

--- CreateObjectExample.ps1
+++ CreateObjectExample.ps1
@@ -5,7 +5,7 @@
     or object specified.
 
     NOTE:  When the object is first created it exists only in memory.  Use the Save() method to 
-            created the object and project permanently on disk.
+            create the object and project permanently on disk.
 
     Use the Add-Member cmdlet to add properties as shown below.
     The script will error if one of the properties you attempt to add already exists.
@@ -16,24 +16,44 @@ clear-host
 $prj = 'Inspector1'
 $obj = 'tester3'
  
+### Load PSNativeDataStore!
+
 Import-Module -Name PSNativeDataStore -DisableNameChecking
+
+
+### Get-NDSObject retrieves an existing object from disk, if it exists
+### If not, it will create an empty NDS object - just add your own properties
 
 $tester3 = Get-NDSObject -projectname $prj -objectname $obj
 
 
-if( -not ($tester3.PSObject.Properties['First'] ) )
+### If object did not exist, it will not contain only default NDS properties
+### In the following section will add any properties that are missing.
+### Note:  If the properties already exist, the script will abort
+### The following examples shows you how to test for the property and assign if missing
+
+
+if( -not ($tester3.PSObject.Properties['First'] ) )  ## test if property exists for this object.
 {
+    ## Add the non-existing property using Add-Member
+
     Add-Member -InputObject $tester3 -MemberType NoteProperty -Name First -Value "This is the first value"
 }
 
-if( -not ($tester3.PSObject.Properties['First'] ) )
+if( -not ($tester3.PSObject.Properties['Second'] ) )
 {
     Add-Member -InputObject $tester3 -MemberType NoteProperty -Name Second -Value 5490393854
 }
 
-if( -not ($tester3.PSObject.Properties['First'] ) )
+
+### Note in this example, we are testing for 'Third' with a capitial T but are creating 'third' with a small 't'
+### This is strictly for example to show you principals of PowerShell  (It will also alert us if Microsoft changes something in future!!!! - because you know they will)
+
+if( -not ($tester3.PSObject.Properties['Third'] ) )
 {
     Add-Member -InputObject $tester3 -MemberType NoteProperty -Name third -Value "And now for something completely different"
 }
 
+
+### Permanently save this object to the Project named 'Inspector1'... feel free to Inspect this directory :)
 $tester3.Save()
--- TransformObject.ps1
+++ TransformObject.ps1
@@ -5,25 +5,35 @@
 ##################################################################################################>
 clear-host
 
-Remove-Module -Name PSNativeDataStore 
+### Load PSNativeDataStore!
+
 Import-Module -Name PSNativeDataStore -DisableNameChecking
+
 
 $prj = 'Inspector1'
 $obj = 'tester4'
+
+### Create an object using the hash table method.  
+### This is a standard PowerShell object and exists only in memory
+### It can not be used with NDS in this state
+
+## define the properties
 $properties = @{
     'BreakFast' = 'burger'
     'Lunch' = 'burger'
     'Supper' = 'I will gladly pay you Tuesday for a hamburger today'
 }
 
-## Create an object
+## Create the object with the properties defined by the hash table object
 $test4 = New-Object –TypeName PSObject –Property $properties
 
-## Convert to a PS Native Data Store object 
+
+### Now we convert to standard PSObject into a PS Native Data Store object
+### This adds additional properties and the 'self-aware' feature
+
 TransformTo-NDSObject -inputobject $test4 -projectname $prj -objectname $obj 
 
-## Now object has ability to save itself
+
+### Now object has ability to save itself!
+
 $test4.Save()
-
-
-
